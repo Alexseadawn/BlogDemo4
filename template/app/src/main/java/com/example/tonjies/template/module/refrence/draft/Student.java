3314d49ff227e2367a8e741fef0517dd5d819d0a
tonjies <1750447997@qq.com>
2018-12-15 13:57:25 +0800

    你好，世界

---
+++ template/app/src/main/java/com/example/tonjies/template/module/refrence/draft/Student.java
@@ -0,0 +1,31 @@
+package com.example.tonjies.template.module.refrence.draft;
+
+/**
+ * Created by 舍长 on 2018/12/14
+ * describe:
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
